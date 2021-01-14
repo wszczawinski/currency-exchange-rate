60450e99059f550296e205eafd13bb13cf34af05
Wojtek Szczawinski <szczawinski.wojtek@gmail.com>
2021-01-14 21:56:01 +0100

    feat(showFavorite): show table of favorite currencies from list

--- src/components/FavoriteCurrencies.jsx
+++ src/components/FavoriteCurrencies.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import styles from './FavoriteCurrencies.module.css';
 
-export function FavoriteCurrencies() {
-  
-
+export function FavoriteCurrencies({ exchangeRates, handleClick }) {
   return (
     <div>
       <h3 className={styles.title}>Favorite Currencies</h3>
+      {typeof exchangeRates === 'object' &&
+        Array.from(exchangeRates).map(rate => {
+          return (
+            rate.favorite === 'favorite' && (
+              <button
+                className={`singleCurrency ${rate.favorite}`}
+                key={rate.code}
+                onClick={() => handleClick(rate)}
+              >
+                <div>{rate.code}</div>
+                <div>{rate.currency}</div>
+                <div>
+                  <p>{rate.bid}</p>
+                  <p>{rate.ask}</p>
+                  <p>{rate.mid}</p>
+                </div>
+              </button>
+            )
+          );
+        })}
     </div>
   );
 }
